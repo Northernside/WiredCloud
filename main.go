4f57f6bd726898c71233676316de04c360c07301
Northernside <gpg@northernsi.de>
2024-09-29 23:49:02 +0200

    / gc interval

--- main.go
+++ main.go
@@ -1,11 +1,21 @@
 package main
 
 import (
+	"runtime"
+	"time"
 	"wiredcloud/api"
 	"wiredcloud/modules/env"
 )
 
 func main() {
 	env.LoadEnvFile()
+
+	go func() {
+		for {
+			runtime.GC()
+			time.Sleep(5 * time.Second)
+		}
+	}()
+
 	api.StartWebServer()
 }
